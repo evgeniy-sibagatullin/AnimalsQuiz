4d8227f01ebeb2b896076caf8a38b5a4a5b8c509
evgeniy-sibagatullin <evgeniy.sibagatullin@gmail.com>
2017-05-20 09:49:25 +0300

    Initial commit.

---
+++ app/src/main/java/com/quiz/android/animalsquiz/Animal.java
@@ -0,0 +1,5 @@
+package com.quiz.android.animalsquiz;
+
+public enum Animal {
+
+}
---
+++ app/src/main/java/com/quiz/android/animalsquiz/Question.java
@@ -0,0 +1,8 @@
+package com.quiz.android.animalsquiz;
+
+/**
+ * Created by Borealis on 05/20/2017.
+ */
+
+public enum Question {
+}
